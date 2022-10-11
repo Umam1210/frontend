6a2934d8e111d07fba307710fae1ef972833c495
khairul umam <khairulumam950@gmail.com>
2022-10-11 10:49:30 +0700

    test5

--- src/Components/CardMovies.jsx
+++ src/Components/CardMovies.jsx
@@ -3,16 +3,30 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import "bootstrap/dist/css/bootstrap.min.css";
-import data from '../data/data.json'
 import { Link } from 'react-router-dom'
-// import image from '../image/card.png'
+
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import { API } from '../config/api'
+import { useQuery } from 'react-query'
 
 function CardMovies() {
+
+    const state = useContext (UserContext)
+    console.log("state", state)
+    let { data: films } = useQuery('filmsCache', async () => {
+        const response = await API.get('/films');
+        // console.log("ini response",response)
+        return response.data.data;
+      });
+
+
+
     return (
         <div className="bg-dark">
             <h4 className="text-light ms-3 pt-3">Movies</h4>
             <Row xs={1} md={6} className="mx-2 bg-dark">
-                {data.filter((item) => item.category_id == 2).map((item, id) => {
+                {films.filter((item) => item.category_id == 2).map((item, id) => {
                     return (
                         <Col className="my-3">
                             <Card>
